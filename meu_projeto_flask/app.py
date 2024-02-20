b8b44614fb3bb967c5ef600c319264fac52cb8c7
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2024-02-19 20:10:49 -0400

    projeto já está incluso pdf gerado, a foto salva no pdf gerado agora falta sobrepor a imagem com as linhas das mãos

--- meu_projeto_flask/app.py
+++ meu_projeto_flask/app.py
@@ -1,21 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from werkzeug.utils import secure_filename
+from flask_mail import Mail, Message
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from random import choice
+from io import BytesIO
+import os
 import sqlite3
 import hashlib
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'  # Chave secreta para sessão
 
-# Função para conectar ao banco de dados
+# Lista de análises de quiromancia pré-definidas
+QUIROMANCY_ANALYSES = [
+    "Análise 1: Você tem uma grande linha da vida, indicando vitalidade.",
+    "Análise 2: Sua linha do coração sugere uma pessoa apaixonada.",
+    "Análise 3: A linha da cabeça revela uma mente analítica.",
+    # Adicione mais análises conforme necessário
+]
+
+# Configuração do app Flask
+app.secret_key = os.environ.get('SECRET_KEY', 'fallback_secret_key')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+app.config.update(
+    MAIL_SERVER='smtp-mail.outlook.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME', 'mayconmspalco@hotmail.com'),
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD', 'Mp06vA29')
+)
+
+mail = Mail(app)
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-# Função para criar a tabela 'users' no banco de dados
 def create_users_table():
     conn = get_db_connection()
     cursor = conn.cursor()
-
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,15 +56,22 @@ def create_users_table():
             password TEXT NOT NULL
         )
     ''')
-
     conn.commit()
     conn.close()
 
-# Rota para a página de cadastro
+@app.route('/')
+def index():
+    if 'logged_in' in session:
+        return redirect(url_for('upload_file'))
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-    error_message = None
-
     if request.method == 'POST':
         nome = request.form['name']
         email = request.form['email']
@@ -41,96 +79,121 @@ def cadastro():
         senha = request.form['password']
         confirmar_senha = request.form['confirm_password']
 
-        # Verifica se as senhas correspondem
         if senha != confirmar_senha:
-            error_message = "As senhas não correspondem. Por favor, tente novamente."
-        else:
-            # Criptografa a senha
-            hashed_password = hashlib.sha256(senha.encode()).hexdigest()
+            return render_template('cadastro.html', error_message="As senhas não correspondem.")
 
-            # Verifica se o e-mail já está cadastrado
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
-            existing_user = cursor.fetchone()
+        hashed_password = hashlib.sha256(senha.encode()).hexdigest()
 
-            if existing_user:
-                error_message = "O e-mail já está cadastrado. Por favor, use outro e-mail."
-            else:
-                # Conectar ao banco de dados e inserir os dados do usuário
-                cursor.execute('INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)',
-                               (nome, email, telefone, hashed_password))
-                conn.commit()
-                conn.close()
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+        if cursor.fetchone():
+            return render_template('cadastro.html', error_message="O e-mail já está cadastrado.")
 
-                # Redirecionar para a página de login após o cadastro
-                return redirect(url_for('login'))
+        cursor.execute('INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)',
+                       (nome, email, telefone, hashed_password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('login'))
 
-    return render_template('cadastro.html', error_message=error_message)
+    return render_template('cadastro.html')
 
-# Rota para a página de login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error_message = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('password')
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user and hashlib.sha256(senha.encode()).hexdigest() == user['password']:
+            session['logged_in'] = True
+            session['email'] = email
+            return redirect(url_for('upload_file'))
+        else:
+            error_message = "Usuário ou senha incorretos."
+            return render_template('login.html', error_message=error_message)
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
 
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '' or not allowed_file(file.filename):
+            return redirect(request.url)
 
-        # Lógica para autenticar o usuário
-        if autenticacao_bem_sucedida(username, password):
-            session['logged_in'] = True
-            return redirect(url_for('index'))
-        else:
-            error_message = 'Usuário ou senha incorretos. Por favor, tente novamente.'
+        # Gera o PDF diretamente da imagem enviada
+        analysis_result = choice(QUIROMANCY_ANALYSES)
+        pdf_path = generate_pdf(analysis_result, file)
+        send_email(pdf_path, session.get('email'))
 
-    return render_template('login.html', error_message=error_message)
+        return 'Arquivo enviado e análise realizada. Verifique seu e-mail.'
+    return render_template('upload.html')
 
-# Rota para verificar se o e-mail já está cadastrado
 @app.route('/verificar_email', methods=['POST'])
 def verificar_email():
-    email = request.json['email']
-
-    # Verificar se o e-mail já está cadastrado
+    data = request.get_json()
+    email = data.get('email')
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM users WHERE email = ?', (email,))
-    existing_user = cursor.fetchone()
+    exists = cursor.fetchone() is not None
     conn.close()
+    return jsonify({'exists': exists})
 
-    if existing_user:
-        return jsonify({'exists': True})
-    else:
-        return jsonify({'exists': False})
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Função para autenticar o usuário (substitua isso com sua própria lógica de autenticação)
-def autenticacao_bem_sucedida(username, password):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users WHERE email = ?', (username,))
-    user = cursor.fetchone()
-    conn.close()
+def generate_pdf(content, file):
+    pdf_filename = os.path.splitext(file.filename)[0] + '.pdf'
+    pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
 
-    if user:
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        if user['password'] == hashed_password:
-            return True
-    return False
+    # Cria um objeto PDF com múltiplas páginas
+    doc = SimpleDocTemplate(pdf_path, pagesize=letter)
+    story = []
 
-# Rota para a página inicial (exemplo)
-@app.route('/')
-@app.route('/home')
-def index():
-    return render_template('home.html')
+    styles = getSampleStyleSheet()
+    normal_style = styles['Normal']
 
-# Função para formatar o número de telefone enquanto o usuário digita
-@app.template_filter('format_phone')
-def format_phone(s):
-    s = ''.join(filter(str.isdigit, s))
-    return '({}) {}-{}'.format(s[:2], s[2:7], s[7:])
+    # Converte a imagem enviada para um objeto que o ReportLab pode usar
+    image = Image(BytesIO(file.read()), width=400, height=200)
+    story.append(image)
 
-# Restante do código...
+    story.append(Spacer(1, 12))
+    story.append(Paragraph("Análise Quiromântica Detalhada:", normal_style))
+    story.append(Spacer(1, 12))
+    analysis_paragraph = Paragraph(content, normal_style)
+    story.append(analysis_paragraph)
+
+    doc.build(story)
+
+    # Retorna o caminho para o PDF gerado
+    return pdf_path
+
+
+def send_email(pdf_path, recipient):
+    if recipient:
+        msg = Message('Sua Análise de Quiromancia', sender=app.config['MAIL_USERNAME'], recipients=[recipient])
+        msg.body = 'Encontre em anexo a análise de sua quiromancia.'
+        with app.open_resource(pdf_path) as fp:
+            msg.attach(pdf_path, 'application/pdf', fp.read())
+        mail.send(msg)
 
 if __name__ == '__main__':
-    create_users_table()  # Garante que a tabela de usuários exista antes de iniciar o aplicativo
+    create_users_table()
     app.run(debug=True)
